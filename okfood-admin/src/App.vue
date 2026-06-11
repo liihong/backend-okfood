a03c2cae5878c49dafff62ab23a792dd4415c844
lihong <510754059@qq.com>
2026-06-11 15:17:41 +0800

    优化小程序样式

--- okfood-admin/src/App.vue
+++ okfood-admin/src/App.vue
@@ -1,19 +1,22 @@
 <script setup>
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import { useToast } from './composables/useToast.js'
 
 const { toastOpen, toastText, toastKind } = useToast()
 </script>
 
 <template>
-  <router-view />
-  <Transition name="toast-fade">
-    <div
-      v-if="toastOpen"
-      class="app-toast"
-      :class="{ 'app-toast--error': toastKind === 'error' }"
-      :role="toastKind === 'error' ? 'alert' : 'status'"
-    >
-      {{ toastText }}
-    </div>
-  </Transition>
+  <el-config-provider :locale="zhCn">
+    <router-view />
+    <Transition name="toast-fade">
+      <div
+        v-if="toastOpen"
+        class="app-toast"
+        :class="{ 'app-toast--error': toastKind === 'error' }"
+        :role="toastKind === 'error' ? 'alert' : 'status'"
+      >
+        {{ toastText }}
+      </div>
+    </Transition>
+  </el-config-provider>
 </template>
